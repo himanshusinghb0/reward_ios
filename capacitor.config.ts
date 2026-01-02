bb06c34f858f6eabda8032cb90de1720e34408eb
Himanshu <8967himanshusingh@gmail>
2026-01-02 12:54:57 +0530

    change the app id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.fizzbleep.jackson',
+  appId: 'com.jackson.rewards.app',
   appName: 'Jackson',
   webDir: 'out',
   server: {
